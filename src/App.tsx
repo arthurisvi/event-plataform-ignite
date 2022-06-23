0c54afbd4c6c29140d8a33774dd448c8c1c2a108
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-06-22 21:01:53 -0300

    feat: creating first components

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query{
-    lessons{
-      id
-      title
-      teacher{
-        name
-      }
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
 
-  const { data } = useQuery<{ lessons: Lesson []}>(GET_LESSONS_QUERY)
-
-  console.log(data);
-
   return (
-    <div>
-      <h1 className = "text-5xl font-bold text-violet-500">Hello, world!</h1>
-      <ul>
-        {data?.lessons.map(lesson => {
-          return (
-            <li key={lesson.id}>{lesson.title}</li>
-          )
-        })}
-      </ul>
-    </div>
+    <Event />
   )
 }
 
