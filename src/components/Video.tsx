b057e5cefec6ffb19c98c39c8b3b20af9a2f7f60
arthurisvi <83462514+arthurisvi@users.noreply.github.com>
2022-06-22 23:44:10 -0300

    feat: finished sidebar and page division

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,3 +1,9 @@
 export function Video() {
-  return <h1>Video</h1>;
+  return (
+    <div className="flex-1">
+      <div className="flex items-center justify-center">
+        <h1>Video</h1>
+      </div>
+    </div>
+  );
 }
